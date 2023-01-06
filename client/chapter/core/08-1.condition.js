17406a703e5e3ce1680f6a52f448af2a4b30cf9b
supermantoman <jwg7632@gmail.com>
2023-01-06 17:02:01 +0900

    includes 함수 복습

--- client/chapter/core/08-1.condition.js
+++ client/chapter/core/08-1.condition.js
@@ -2,22 +2,33 @@
 /* Condition                                                              */
 /* ---------------------------------------------------------------------- */
 
-let number = prompt('숫자를 입력해주세요', 0);
+let number = prompt('숫자를 입력해 주세요',0);
 
-if (number > 0) {
-  console.log('1');
-} else if(number <=0) {
-  console.log(-1);
-} else {
-  console.log('Nothing');
-}
 
-let message =
-(number > 0 ) ? '1' :
-(number < 0) ? '-1' :
+let message = 
+(number > 0) ? '1' : 
+(number < 0) ? '-1': 
 '아무것도 아닙니다.';
 
 console.log(message);
+
+
+
+/* 
+if(number > 0){
+  
+}else if(number < 0){
+  
+}else{
+  
+}
+ */
+
+
+
+
+
+
 
 // 그 영화 봤니?
 //     ↓
@@ -32,28 +43,43 @@ let didWatchMovie = 'yes';
 // 영화 볼거니?
 let goingToWatchMovie = 'no';
 
-if(didWatchMovie === 'yes') {
-  console.log('그거 잼');
-} else if (goingToWatchMovie === 'yes') {
-  console.log('너무 설렌다');
-} else {
-  console.log(난 별로. ㅎㅋ);
+
+
+if(didWatchMovie.includes('yes')){ // if 문(statement)
+  console.log('그거 재밌더라..?');
+
+}else if(goingToWatchMovie === 'yes'){ // else if 복수 조건 처리
+  console.log('너무 설렌다~~');
+
+}else{ // else 절(caluse)
+  console.log('음...난 별루');
 }
 
 
-// if 문(statement)
-if(...) {
 
-} else if(...){
 
-}else{
+let movieMessage = 
+(didWatchMovie.includes('yes')) ? '그거 재밌더라' :
+(goingToWatchMovie === 'yes') ? '너무 설렌다~~' : '난 별루...'
 
-}
 
-// else 절(caluse)
 
-// else if 복수 조건 처리
+
 
 // 조건부 연산자
 
 // 멀티 조건부 연산자 식
+
+
+
+
+
+
+
+
+
+
+
+
+
+
