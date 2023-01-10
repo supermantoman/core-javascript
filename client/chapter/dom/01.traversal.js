8b7fa1598ea1bba1b7b50a4d4bd0c26886000fa6
supermantoman <jwg7632@gmail.com>
2023-01-10 10:13:16 +0900

    nodeClass.js 추가

--- client/chapter/dom/01.traversal.js
+++ client/chapter/dom/01.traversal.js
@@ -2,8 +2,6 @@
 /* DOM traversal                                                          */
 /* ---------------------------------------------------------------------- */
 
-const { getRandomSeed } = require('canvas-sketch-util/random');
-
 
 /* 모든 노드에서 사용 */
 // - parentNode
@@ -29,20 +27,65 @@ const { getRandomSeed } = require('canvas-sketch-util/random');
 // - querySelectorAll
 // - closest
 
-/* 문서 대상 확인 */
-// - matches
 
 // let first = document.querySelector('.first');
 
-let span = document.querySelectorAll('span');
-
-console.log(span);
-
-let [first, second] = document.querySelectorAll('span');
-
-console.log(second);
-
-getRandomNode('.first')
+// let [first,second] = document.querySelectorAll('span');
 
 
+// console.log(second);
+
+
+
+
+
+
+console.log();
+
+
+
+
+
+
+/* 문서 대상 확인 */
+// - matches
+//            선택자 안에  class || id 를 가지고 있는 대상이 있어?
+console.log( getNode('.first').matches('.first') );
 // - contains
+
+
+//              선택자의 자식들 중에 해당 element가 있어?
+console.log( getNode('h1').contains(getNode('.first')) );
+
+
+
+
+
+
+
+
+let first = getNode('.first');
+
+let clicked = false;
+document.addEventListener('click',()=>{
+
+  
+  if(first.classList.contains('first') && !clicked){
+    first.classList.add('is-active')
+  }else{
+    first.classList.remove('is-active')
+  }
+
+  clicked = !clicked;
+})
+
+
+
+
+
+
+
+
+
+
+
